acedf66a68201d127ff31171e0d9a2caa3612e3c
sapna768 <gupta345621@gmail.com>
2026-09-06 15:55:40 +0530

    update app.py with latest update

--- app.py
+++ app.py
@@ -202,5 +202,6 @@ if __name__ == "__main__":
         description=f"Ask questions about {name}'s career, projects, and expertise.",
         
     )
-    demo.launch(server_port=7861)
+    
+    demo.launch(server_name="0.0.0.0", server_port=int(os.environ.get("PORT", 7860)))
 
